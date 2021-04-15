e4ea243d7620cbf920d94e6204941f9daa6a4947
Sona0817 <dlarkgus963@gmail.com>
2021-04-15 15:43:04 +0900

    Update LanTripDAO.java

--- src/main/java/com/tjoeunit/biz/lantrip/Impl/LanTripDAO.java
+++ src/main/java/com/tjoeunit/biz/lantrip/Impl/LanTripDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
 import com.tjoeunit.biz.lantrip.LanTripVO;
 
 @Repository
