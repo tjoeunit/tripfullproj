d7cc14a4409834aa6a1a6cd374b633306715af83
kimj7737 <rlamawkdth@naver.com>
2021-04-15 16:57:37 +0900

    rewa

---
+++ src/main/java/com/tjoeunit/biz/story/StoryService.java
@@ -0,0 +1,21 @@
+package com.tjoeunit.biz.story;
+
+import java.util.List;
+
+public interface StoryService {
+	
+	//글 등록
+	int insertStory(StoryVO vo);
+	
+	//글 목록 조회
+	List<StoryVO> getStoryList(StoryVO vo);
+	
+	//글 상세 조회
+	StoryVO getStory(StoryVO vo);
+	
+	//글 수정
+	void updateStory(StoryVO vo);
+	
+	//글 삭제
+	void deleteStory(StoryVO vo);
+}
