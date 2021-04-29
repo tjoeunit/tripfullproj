d7432e1e5483d3706f194066aa25095765e6b1d6
leta99p <66706712+leta99p@users.noreply.github.com>
2021-04-30 00:55:50 +0900

    tripfull

--- src/main/java/com/tjoeunit/biz/story/StoryReplyService.java
+++ src/main/java/com/tjoeunit/biz/story/StoryReplyService.java
@@ -13,7 +13,9 @@ public interface StoryReplyService {
     
 //댓글 삭제
     public void deleteReply(StoryReplyVO rvo);  
-	 
+
+    //댓글 번호를 통해 댓글 작성자의 아이디를 추출
+    public String selectIdByReplyNo(int reply_no);
     /*
 	  //선택된 댓글 조회 public StoryReplyVO selectStoryReply(StoryReplyVO rvo);
 	 */
